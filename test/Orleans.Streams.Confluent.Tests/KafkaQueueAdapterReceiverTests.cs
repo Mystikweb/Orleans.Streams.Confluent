15e48518721c0d6d2ce45164ebada441887093d6
Christopher Hair <mystikweb@live.ca>
2026-09-22 13:21:05 -0400

    Add unit tests for cancellation handling in KafkaQueueAdapterReceiver

---
+++ test/Orleans.Streams.Confluent.Tests/KafkaQueueAdapterReceiverTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orleans.Streams;
+
+namespace Orleans.Streams.Confluent.Tests;
+
+[TestClass]
+public sealed class KafkaQueueAdapterReceiverTests
+{
+    [TestMethod]
+    public async Task GetQueueMessagesAsync_WhenCancellationRequested_ThrowsOperationCanceledException()
+    {
+        using var cancellationTokenSource = new CancellationTokenSource();
+        cancellationTokenSource.Cancel();
+        var receiver = CreateReceiver();
+
+        Func<Task> act = async () => await receiver.GetQueueMessagesAsync(10, cancellationTokenSource.Token);
+
+        await act.Should().ThrowAsync<OperationCanceledException>();
+    }
+
+    [TestMethod]
+    public async Task MessagesDeliveredAsync_WhenCancellationRequested_ThrowsOperationCanceledException()
+    {
+        using var cancellationTokenSource = new CancellationTokenSource();
+        cancellationTokenSource.Cancel();
+        var receiver = CreateReceiver();
+
+        Func<Task> act = async () => await receiver.MessagesDeliveredAsync([], cancellationTokenSource.Token);
+
+        await act.Should().ThrowAsync<OperationCanceledException>();
+    }
+
+    private static KafkaQueueAdapterReceiver CreateReceiver()
+        => new(
+            "provider",
+            null!,
+            null!,
+            NullLogger<KafkaQueueAdapterReceiver>.Instance,
+            default,
+            "consumer-group");
+}
